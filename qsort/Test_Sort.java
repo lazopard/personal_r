8af63f957852bb196fde2a1658b4d052732e94b4
Leonardo Azopardo <lazopard@gmail.com>
2014-06-27 13:14:01 -0400

    Tested both double and int qsorts

--- qsort/Test_Sort.java
+++ qsort/Test_Sort.java
@@ -60,15 +60,17 @@ public class Test_Sort {
     private final static double EPSILON = 0.00001;
 
     // Singleton qsort with peto remark for doubles.
-    public static void dbQSort(double[] v, int left, int right) {
+    public static void dbQSort(double[] v, int l, int r) {
 
-        int pivot;
+        double pivot;
         int m = l + (int) (Math.random() * ((r - l) + 1)); // random number between first and last, Peto's remark
 
         //median(v[l], v[m], v[r])
-        float lLEm = ((v[l] - v[m]) <= 0.0F) ? 0.0F -(v[l] - v[m]) : (v[l] - v[m]);
-        float mLEr = ((v[m] - v[r]) <= 0.0F) ? 0.0F -(v[m] - v[r]) : (v[m] - v[r]);
-        float lLEr = ((v[l] - v[r]) <= 0.0F) ? 0.0F -(v[l] - v[r]) : (v[l] - v[r]);
+
+        //float diffLM = ((v[l] - v[m]) <= 0.0F) ? 0.0F - (v[l] - v[m]) : (v[l] - v[m]);
+        //float diffMR = ((v[m] - v[r]) <= 0.0F) ? 0.0F - (v[m] - v[r]) : (v[m] - v[r]);
+        //float diffLR = ((v[l] - v[r]) <= 0.0F) ? 0.0F - (v[l] - v[r]) : (v[l] - v[r]);
+
         if (v[l] <= v[m]) {
             if (v[m] <= v[r])
                 pivot = v[m];
@@ -90,7 +92,7 @@ public class Test_Sort {
         }
 
         int i=l,j=r;
-        int tmp; //tmp variable for swapping
+        double tmp; //tmp variable for swapping
 
         //Partioning
         while(i<=j) {
@@ -116,9 +118,50 @@ public class Test_Sort {
 
     }
 
+    public static boolean isSorted(int[] a) {
+        for(int i = 0; i < a.length-1; i ++) { 
+            if (a[i] > a[i+1]) {
+                return false; 
+            }
+        }
+        return true;
+    }
+
+    public static boolean dIsSorted(double [] a) {
+
+        for(int i = 0; i < a.length-1; i ++) { 
+            System.out.println(a[i]);
+            if (a[i] >= a[i+1]) {
+                return false; 
+            }
+        }
+        return true;
+    }
+
     public static void main(String [] args) {
-        int []i = { 2, 3, 1, 10, 5, 89, 91, 32, 2, 4, 5,  78, 90, 100, 31 };
-        intQSort(i, 0, i.length - 1);
-        for(int j= 0; j < i.length; j++) System.out.println(i[j]);
+        int n = 10000000;
+        int [] i = new int[n];
+        double [] d = new double[n];
+        for(int j = 0; j < n; j++) {
+            //i[j] = 0 + (int) (Math.random()*((n - 0)+ 1 ));
+            //d[j] = 0 + Math.random()*((n - 0)+ 1 );
+            d[j] = Math.random();
+        }
+
+        //long iSortTimeStart = System.nanoTime();
+        //intQSort(i, 0, n - 1);
+        //long iSortTimeEnd = System.nanoTime();
+        //long iSortDuration = iSortTimeEnd - iSortTimeStart;
+
+        long dSortTimeStart = System.nanoTime();
+        dbQSort(d, 0, n - 1);
+        long dSortTimeEnd = System.nanoTime();
+        long dSortDuration = dSortTimeEnd - dSortTimeStart;
+
+        //System.out.println("iQsort time: " + iSortDuration + "\n");
+        System.out.println("dbQSort time: " + dSortDuration + "\n");
+
+        //System.out.println("Is int array sorted: ?" + isSorted(i));
+        System.out.println("Is double array sorted: ?" + dIsSorted(d));
     }
 }
